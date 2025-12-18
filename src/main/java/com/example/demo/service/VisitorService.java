9d8569821a1a6e90e48d652e102c0e51949f3b86
bala-amypo <bala@amypo.in>
2025-12-18 10:14:30 +0000

    User commit

--- src/main/java/com/example/demo/service/VisitorService.java
+++ src/main/java/com/example/demo/service/VisitorService.java
@@ -1,5 +1,10 @@
 package com.example.demo.service;
 
+import com.example.demo.model.*;
+import java.util.*;
+
 public interface VisitorService {
-    
+    Visitor createVisitor(Visitor visitor);
+    Visitor getVisitor(Long id);
+    List<Visitor> getAllVisitors();
 }
--- src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VisitorServiceImpl.java
@@ -1,4 +1,4 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
 import com.example.demo.model.*;
 import com.example.demo.repository.*;
@@ -6,22 +6,25 @@ import org.springframework.stereotype.*;
 import java.util.*;
 
 @Service
-public class VisitorServiceImpl implements Visitor{
+public class VisitorServiceImpl implements VisitorService {
 
     private final VisitorRepository visitorRepository;
 
-    public VisitorService(VisitorRepository visitorRepository) {
+    public VisitorServiceImpl(VisitorRepository visitorRepository) {
         this.visitorRepository = visitorRepository;
     }
 
+    @Override
     public Visitor createVisitor(Visitor visitor) {
         return visitorRepository.save(visitor);
     }
 
+    @Override
     public Visitor getVisitor(Long id) {
         return visitorRepository.findById(id).orElse(null);
     }
 
+    @Override
     public List<Visitor> getAllVisitors() {
         return visitorRepository.findAll();
     }
