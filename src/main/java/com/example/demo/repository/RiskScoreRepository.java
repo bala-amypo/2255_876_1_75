a4978c525f562b9963dddfcde769dd600439d793
bala-amypo <bala@amypo.in>
2025-12-18 08:55:58 +0000

    User commit

--- src/main/java/com/example/demo/repository/RiskScoreRepository.java
+++ src/main/java/com/example/demo/repository/RiskScoreRepository.java
@@ -3,9 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.VisitLog;
+import com.example.demo.model.RiskScore;
 
 @Repository
-public interface VisitLogRepository extends JpaRepository<VisitLog,Long>{
+public interface RiskScoreRepository extends JpaRepository<RiskScore,Long>{
 
 }
--- src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
+++ src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
@@ -3,9 +3,9 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.VisitLog;
+import com.example.demo.model.ScoreAuditLog;
 
 @Repository
-public interface VisitLogRepository extends JpaRepository<VisitLog,Long>{
+public interface ScoreAuditLogRepository extends JpaRepository<ScoreAuditLog,Long>{
 
 }
