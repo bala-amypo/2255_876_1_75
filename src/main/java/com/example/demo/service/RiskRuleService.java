474cefa5ed154aadad5ac024bf5083cc2208e306
bala-amypo <bala@amypo.in>
2025-12-18 15:03:58 +0000

    User commit

--- src/main/java/com/example/demo/service/RiskRuleService.java
+++ src/main/java/com/example/demo/service/RiskRuleService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.*;
 import java.util.*;
 
-public interface RiskRule {
+public interface RiskRuleService {
     RiskRule createRule(RiskRule rule);
     List<RiskRule> getAllRules();
     RiskRule getRule(Long id);
--- src/main/java/com/example/demo/service/impl/RiskRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RiskRuleServiceImpl.java
@@ -1,7 +1,8 @@
-package com.example.demo.service;
+package com.example.demo.service.impl;
 
 import com.example.demo.model.*;
 import com.example.demo.repository.*;
+import com.example.demo.service.*;
 import org.springframework.stereotype.*;
 import java.util.*;
 
