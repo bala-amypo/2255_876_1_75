4dc850d64062188c66e4ae3d2d08a2c4d0503284
bala-amypo <bala@amypo.in>
2025-12-18 04:55:46 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskRule.java
+++ src/main/java/com/example/demo/model/RiskRule.java
@@ -1,3 +1,11 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
+@Entity
+public class RiskRule {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -5,4 +13,52 @@
     private String ruleType;
     private int threshold;
     private int scoreImpact;
-    private LocalTimeDate 
+    private LocalDateTime createdAt;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getRuleName() {
+        return ruleName;
+    }
+    public void setRuleName(String ruleName) {
+        this.ruleName = ruleName;
+    }
+    public String getRuleType() {
+        return ruleType;
+    }
+    public void setRuleType(String ruleType) {
+        this.ruleType = ruleType;
+    }
+    public int getThreshold() {
+        return threshold;
+    }
+    public void setThreshold(int threshold) {
+        this.threshold = threshold;
+    }
+    public int getScoreImpact() {
+        return scoreImpact;
+    }
+    public void setScoreImpact(int scoreImpact) {
+        this.scoreImpact = scoreImpact;
+    }
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+    public RiskRule(long id, String ruleName, String ruleType, int threshold, int scoreImpact,
+            LocalDateTime createdAt) {
+        this.id = id;
+        this.ruleName = ruleName;
+        this.ruleType = ruleType;
+        this.threshold = threshold;
+        this.scoreImpact = scoreImpact;
+        this.createdAt = createdAt;
+    }
+    public RiskRule() {
+    }
+}
