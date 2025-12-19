dd5864e074365ca4ed83614dcb4d65f85e28cab0
bala-amypo <bala@amypo.in>
2025-12-19 10:14:17 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,36 +1,26 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UserService;
 import com.example.demo.dto.*;
-import com.example.demo.model.*;
-import com.example.demo.repository.*;
-import com.example.demo.service.*;
-import org.springframework.stereotype.Service;
-import java.util.*;
+import com.example.demo.model.User;
 
-@Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    public UserServiceImpl() {
-    }
-    
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
     @Override
     public User register(RegisterRequest request) {
-        return User.builder()
-                .email(request.getEmail())
-                .password(request.getPassword())
-                .role(Set.of("ROLE_USER"))
-                .build();
+        return null;
     }
 
     @Override
     public AuthResponse login(AuthRequest request) {
-        return new AuthResponse("dummy-token", request.getEmail(), Set.of("ROLE_USER"));
+        return null;
     }
 
     @Override
