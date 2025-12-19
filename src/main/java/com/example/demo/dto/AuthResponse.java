0d2221a165e9369e9f62f9468dc42a12add8db14
bala-amypo <bala@amypo.in>
2025-12-19 05:40:22 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,7 +1,4 @@
 package com.example.demo.dto;
 
 public class AuthResponse {
-    private String token;
-    private String email;
-    private Set<String> roles;
 }
--- src/main/java/com/example/demo/security/CustomUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomUserDetailsService.java
@@ -1,29 +1,4 @@
 package com.example.demo.security;
 
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
-import org.springframework.security.core.userdetails.*;
-import org.springframework.stereotype.Service;
-import java.util.stream.Collectors;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-
-    private final UserRepository userRepository;
-
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
-
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getEmail())
-                .password(user.getPassword())
-                .authorities(user.getRole().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()))
-                .build();
-    }
+public class CustomUserDetailsService{
 }
