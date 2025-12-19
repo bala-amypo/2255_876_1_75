d5bc3419698e586fd7b1c71a0c9e9b6b54379bbc
bala-amypo <bala@amypo.in>
2025-12-19 12:46:21 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,15 +1,47 @@
 package com.example.demo.dto;
 
-import lombok.*;
 import java.util.Set;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class AuthResponse {
 
     private String token;
-    private String email;
+    private String type;
     private Set<String> roles;
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public AuthResponse(String token, String type, Set<String> roles) {
+        this.token = token;
+        this.type = type;
+        this.roles = roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<String> roles) {
+        this.roles = roles;
+    }
 }
