b97c8a3fb82ecc8ac44e49695397508598044d36
bala-amypo <bala@amypo.in>
2025-12-19 05:13:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
+++ src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.model.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
+import java.util.*;
+import com.example.demo.exception.ResourceNotFoundException;
+
 
 @Repository
 public interface ScoreAuditLogRepository extends JpaRepository<ScoreAuditLog, Long> {
